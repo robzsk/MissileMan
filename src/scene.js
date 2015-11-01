a74ab9ce8cf9afa6126f6f46f99d46b1dc53a391
robzsk <rob_zoszak@yahoo.com.au>
2015-11-02 06:44:01 +1100

    soft edge for camera position

--- src/scene.js
+++ src/scene.js
@@ -1,5 +1,46 @@
 'use strict';
 
+// returns a soft edge for the camera position looking at the world
+var edge = function () {
+  var easing = require('bezier-easing'),
+    easeDown = easing(0.585, 0.000, 0.940, 0.805),
+    easeUp = easing(0.105, 0.510, 0.560, 1.000),
+    ret = new THREE.Vector2(),
+    pad = 6,
+    dimensions = new THREE.Vector2(),
+    hard = new THREE.Vector2(),
+    minsoft = new THREE.Vector2(),
+    maxsoft = new THREE.Vector2();
+
+  var edge = {
+    set: function (width, height) {
+      dimensions.set(width, height);
+      hard.set(14, 14);
+      minsoft.set(hard.x + pad, hard.y + pad);
+      maxsoft.set(dimensions.x - hard.x - pad, dimensions.y - hard.y - pad);
+    },
+    get: function (v) {
+      ret.set(v.x, v.y);
+      if (ret.x < minsoft.x) {
+        ret.x = hard.x + easeDown.get(ret.x / minsoft.x) * pad;
+      }
+      if (ret.x > maxsoft.x) {
+        ret.x = maxsoft.x + (easeUp.get((ret.x - maxsoft.x) / minsoft.x)) * pad;
+      }
+      if (ret.y < minsoft.y) {
+        ret.y = hard.y + easeDown.get(ret.y / minsoft.y) * pad;
+      }
+      if (ret.y > maxsoft.y) {
+        ret.y = maxsoft.y + (easeUp.get((ret.y - maxsoft.y) / minsoft.y)) * pad;
+      }
+      return ret;
+    }
+  };
+  // defaults
+  edge.set(64, 53);
+  return edge;
+}();
+
 var _ = require('underscore');
 
 const zoom = 26;
@@ -31,8 +72,12 @@ var Scene = function () {
   };
 
   this.follow = function (p) {
-    cam.position.set(p.x, p.y, zoom);
-  };
+    var e;
+    return function (p) {
+      e = edge.get(p);
+      cam.position.set(e.x, e.y, zoom);
+    };
+  }();
 
   this.remove = function (m) {
     scene.remove(m);
