94b18c90030414c2ba4e3ca2723903b3e196f4c8
robzsk <rob_zoszak@yahoo.com.au>
2015-12-13 10:49:13 +1100

    fixed level modules for browserify

--- src/levels/index.js
+++ src/levels/index.js
@@ -1,8 +1,13 @@
 'use strict';
 
+var levels = [
+	require('./data/level_0'),
+	require('./data/level_1')
+];
+
 module.exports = {
 	load: function (n) {
-		return require('./data/' + n)();
+		return levels[n]();
 	},
 	total: function () {
 		return 2;
