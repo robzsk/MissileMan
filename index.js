08057a3821a514466d3ce9f468db711be304177f
robzsk <rob_zoszak@yahoo.com.au>
2016-01-04 20:43:11 +1100

    removed comments

--- index.js
+++ index.js
@@ -5,18 +5,12 @@ var app = require('app'),
 
 app.on('ready', function (n) {
   var mainWindow = new BrowserWindow({
-    // frame: false,
-    // toolbar: false,
-    // 'accept-first-mouse': true,
     transparent: true,
-    // 'skip-taskbar': true,
     'auto-hide-menu-bar': true,
     'enable-larger-than-screen': true,
     width: 800,
     height: 600
   });
-  // mainWindow.setMenuBarVisibility(false);
-  // mainWindow.setAutoHideMenuBar(true);
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
   client.create(mainWindow);
 });
